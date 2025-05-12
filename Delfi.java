bd6a8bac25d77c28e2ce1c53d28d7048d723a967
Mihkel-Tuisk <mihkel.tuisk@ut.ee>
2025-05-12 11:02:31 +0300

    Esimene osa rühmatööst

---
+++ Delfi.java
@@ -0,0 +1,122 @@
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+    Klassi eesmärk:     Saada informatsiooni Delfi uudisteportaalist.
+    Tähtsamad Meetodid: List<Uudis> leiaKõikUudised() ja void päriUudiseSisu(Uudis)
+*/
+public class Delfi {
+    private String html;
+    private final List<String> keelatedUudiseDomeenid = Arrays.asList(
+            "omamaitse.delfi.ee", "lemmikloom.delfi.ee",
+            "epl.delfi.ee" // 'paywalli' taga
+    );
+
+    /**
+     * Pärib Delfi pealehekülje HTML sisu.
+     *
+     * Kui päring ebaõnnestub, siis trükitakse veateade ja HTML jääb tühjaks.
+     */
+    private void päriHtml() {
+        String link = "https://www.delfi.ee/viimased";
+        Skraaper skraaper = new Skraaper(link);
+        try {
+            html = skraaper.getHtml();
+        } catch (IOException e) {
+            System.out.println("Viga uudiseportaali \"" + link + "\" pealehekülje pärimisel: " + e.getMessage());
+            html = "";
+        }
+    }
+
+    /**
+     * Leiab kõik Delfi pealeheküljelt saadaval olevad uudised.
+     *
+     * @return List<Uudis> Kõik Delfi uudised, kus iga uudis sisaldab linki ja pealkirja.
+     */
+    public List<Uudis> leiaKõikUudised() {
+        päriHtml();
+
+        List<Uudis> uudised = new ArrayList<>();
+
+        String algusHtmlTag = "<article data-";
+        String lõppHtmlTag = "</article>";
+
+        int indeks = 0;
+        while ((indeks = html.indexOf(algusHtmlTag, indeks)) != -1) {
+            int lõpuindeks = html.indexOf(lõppHtmlTag, indeks);
+            if (lõpuindeks != -1) {
+                String htmlInformatsioon = html.substring(indeks + algusHtmlTag.length(), lõpuindeks);
+
+                String pealkiri = Utils.leiaAlaSõne(htmlInformatsioon, " loading=\"lazy\" alt=\"", "\"></picture>");
+
+                String link = Utils.leiaAlaSõne(htmlInformatsioon, "m-h6\"><a href=\"", "\">");
+
+                if (keelatedUudiseDomeenid.stream().anyMatch(link::contains)) {
+                    indeks = lõpuindeks;
+                    continue;
+                }
+
+                if (!link.contains("https://")) {
+                    link = "https://www.delfi.ee" + link;
+                }
+
+                // Lisame uue uudise.
+                uudised.add(new Uudis(link, Utils.lihtsustaHtmlSildid(pealkiri)));
+
+
+                indeks = lõpuindeks;
+            }
+        }
+
+        System.out.println("Kokku uudiseid: " + uudised.size());
+
+        return uudised;
+    }
+
+    /**
+     * Pärib konkreetse uudise sisu, et saada kogu uudise täisteksti.
+     *
+     * @param uudis Uudis objekt, mille sisu tuleb pärida.
+     */
+    public void päriUudiseSisu(Uudis uudis) {
+        // Igat uudist korraga ei ole mõtet pärida, see võtab palju aega ja ka uudiseportaal võib meid "ratelimitida"
+        uudis.päriSisu();
+
+        String sisuHtml = uudis.getSisuHTML();
+
+        StringBuilder uudiseSisu = new StringBuilder();
+        String algusHtmlTag = "<div class=\"fragment fragment-html fragment-html--paragraph\">";
+        String lõppHtmlTag = "</div>";
+
+        int index = 0;
+        while ((index = sisuHtml.indexOf(algusHtmlTag, index)) != -1) {
+            int endIndex = sisuHtml.indexOf(lõppHtmlTag, index);
+            if (endIndex != -1) {
+                String htmlInformatsioon = sisuHtml.substring(index + algusHtmlTag.length(), endIndex);
+
+                String algusP = "<p>";
+                String lõppP = "</p>";
+
+                int pIndex = 0;
+                while ((pIndex = htmlInformatsioon.indexOf(algusP, pIndex)) != -1) {
+                    int pEndIndex = htmlInformatsioon.indexOf(lõppP, pIndex);
+                    if (pEndIndex != -1) {
+                        String pSisu = htmlInformatsioon.substring(pIndex + algusP.length(), pEndIndex);
+
+                        pSisu = Utils.lihtsustaHtmlSildid(pSisu);
+
+                        uudiseSisu.append(pSisu).append("\n");
+
+                        pIndex = pEndIndex + lõppP.length();
+                    }
+                }
+
+                index = endIndex + lõppHtmlTag.length();
+            }
+        }
+
+        uudis.setSisu(uudiseSisu.toString());
+    }
+}
---
+++ Utils.java
@@ -0,0 +1,76 @@
+import java.util.Random;
+
+/**
+    Klassi eesmärk:     Abistada teise klasse, ja vätida koodijuppide kordamist.
+    Tähtsamad Meetodid: String lihtsustaHtmlSildid(String tekst) ja String leiaAlaSõne(String sisu, String alguseMärge, String lõpuMärge)
+*/
+public class Utils {
+    private static final Random random = new Random();
+
+    /**
+     * Tagastab suvalise arvu vahemikus 0 (kaasa arvatud) ja antud maksimum (kaasa arvatud).
+     *
+     * @param maksimum Ülemine piir, milleni arve saab genereerida.
+     * @return Suvaline arv vahemikus 0 kuni maksimum.
+     */
+    static int saaSuvalineArv(int maksimum) {
+        return random.nextInt(maksimum);
+    }
+
+    /**
+     * Otsib ja tagastab ala-sõne, mis asub antud sisu sees, mis algab antud alguse-märgist
+     * ja lõpeb antud lõpu-märgiga.
+     *
+     * @param sisu Tekst, millest ala-sõne otsitakse.
+     * @param alguseMärge Märge, millest ala-sõne otsing algab.
+     * @param lõpuMärge Märge, mille järgi otsing lõpeb.
+     * @return Ala-sõne, mis asub alguse ja lõpu märgi vahel, või tühi string, kui ei leita.
+     */
+    static String leiaAlaSõne(String sisu, String alguseMärge, String lõpuMärge) {
+        int algus = sisu.indexOf(alguseMärge);
+
+        if (algus != -1) {
+
+            algus += alguseMärge.length();
+
+            int lõpp = sisu.indexOf(lõpuMärge, algus);
+
+            if (lõpp != -1) {
+                return sisu.substring(algus, lõpp);
+            }
+        }
+
+        return "";
+    }
+
+    /**
+     * Lihtsustab HTML sildid lihtsustatud tekstiks, eemaldades HTML sildid ja teisendades
+     * HTML entiteedid vastavateks erimärkideks.
+     *
+     * @param tekst Tekst, mis sisaldab HTML silte.
+     * @return Tekst, kus HTML sildid on eemaldatud ja on asendatud vastavate sümbolitega.
+     */
+    static String lihtsustaHtmlSildid(String tekst) {
+        tekst = tekst.replaceAll("<a href=\"([^\"]+)\">([^<]+)</a>", "$2 ($1)");
+        tekst = tekst.replaceAll("<strong>|<b>", "").replaceAll("</strong>|</b>", "");
+        tekst = tekst.replaceAll("<em>", "").replaceAll("</em>", "");
+        tekst = tekst.replaceAll("<[^>]+>", "");
+
+        // https://www.w3schools.com/html/html_entities.asp
+        tekst = tekst.replaceAll("&nbsp;", " ");
+        tekst = tekst.replaceAll("&lt;", "<");
+        tekst = tekst.replaceAll("&gt;", ">");
+        tekst = tekst.replaceAll("&amp;", "&");
+        tekst = tekst.replaceAll("&quot;", "\"");
+        tekst = tekst.replaceAll("&apos;", "'");
+        tekst = tekst.replaceAll("&cent;", "¢");
+        tekst = tekst.replaceAll("&pound;", "£");
+        tekst = tekst.replaceAll("&yen;", "¥");
+        tekst = tekst.replaceAll("&euro;", "€");
+        tekst = tekst.replaceAll("&copy;", "©");
+        tekst = tekst.replaceAll("&reg;", "®");
+        tekst = tekst.replaceAll("&trade;", "™");
+
+        return tekst;
+    }
+}
