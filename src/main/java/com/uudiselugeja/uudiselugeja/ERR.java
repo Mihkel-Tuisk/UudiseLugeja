f5772eaa7fd772f29c50b18598592163b3f4e5a0
Mihkel Tuisk <mihkel.tuisk@ut.ee>
2025-05-21 17:20:14 +0300

    Add files via upload

---
+++ src/main/java/com/uudiselugeja/uudiselugeja/ERR.java
@@ -0,0 +1,131 @@
+package com.uudiselugeja.uudiselugeja;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+    Klassi eesmärk:     Saada informatsiooni ERR uudisteportaalist.
+    Tähtsamad Meetodid: List<Uudis> leiaKõikUudised() ja void päriUudiseSisu(Uudis)
+ */
+public class ERR {
+    private String html;
+    private final List<String> lubatudUudiseDomeenid = Arrays.asList(
+            "www.err.ee", "novaator.err.ee", "eeter.err.ee", "kultuur.err.ee"
+    );
+
+    /**
+     * Pärib ERR pealehekülje HTML sisu.
+     *
+     * Kui päring ebaõnnestub, siis trükitakse veateade ja HTML jääb tühjaks.
+     */
+    private void päriHtml() {
+        String link = "https://www.err.ee/";
+        Skraaper skraaper = new Skraaper(link);
+        try {
+            html = skraaper.getHtml();
+        } catch (IOException e) {
+            System.out.println("Viga uudiseportaali \"" + link + "\" pealehekülje pärimisel: " + e.getMessage());
+            html = "";
+        }
+    }
+
+    /**
+     * Leiab kõik ERR pealeheküljelt saadaval olevad uudised.
+     *
+     * @return List<Uudis> Kõik ERR uudised, kus iga uudis sisaldab linki ja pealkirja.
+     */
+    public List<Uudis> leiaKõikUudised() {
+        päriHtml();
+
+        List<Uudis> uudised = new ArrayList<>();
+
+        String algusHtmlTag = "<div class=\"article\"";
+        String lõppHtmlTag = "</article>";
+
+        int indeks = 0;
+        while ((indeks = html.indexOf(algusHtmlTag, indeks)) != -1) {
+            int lõpuIndeks = html.indexOf(lõppHtmlTag, indeks);
+            if (lõpuIndeks != -1) {
+                String htmlInformatsioon = html.substring(indeks + algusHtmlTag.length(), lõpuIndeks);
+
+                String pealkiri = Utils.leiaAlaSõne(htmlInformatsioon, "aria-label=\"", "\" href=\"");
+
+                if (pealkiri.contains("\" class=\"")) {
+                    pealkiri = pealkiri.replace("\" class=\"", "");
+                }
+
+                // Kui pealkiri on tühi või ei sobi, siis jätame selle artikli vahele
+                if (pealkiri.equals("ERR.ee materjalide kasutustingimused") || pealkiri.isEmpty()) {
+                    indeks = lõpuIndeks;
+                    continue;
+                }
+
+                // Kõik ei ole uudised, mõned on reklaamid https://jupiter.err.ee/ jaoks.
+                String link = Utils.leiaAlaSõne(htmlInformatsioon, "href=\"//", "\"><span class=");
+
+                if (lubatudUudiseDomeenid.stream().noneMatch(link::contains)) {
+                    indeks = lõpuIndeks;
+                    continue;
+                }
+
+                if (!link.contains("https://")) {
+                    link = "https://" + link;
+                }
+
+                // Lisame uue uudise.
+                uudised.add(new Uudis(link, Utils.lihtsustaHtmlSildid(pealkiri)));
+
+                indeks = lõpuIndeks;
+            }
+        }
+
+        return uudised;
+    }
+
+    /**
+     * Pärib konkreetse uudise sisu, et saada kogu uudise täisteksti.
+     *
+     * @param uudis Uudis objekt, mille sisu tuleb pärida.
+     */
+    public void päriUudiseSisu(Uudis uudis) {
+        // Igat uudist korraga ei ole mõtet pärida, see võtab palju aega ja ka uudiseportaal võib meid "ratelimitida"
+        uudis.päriSisu();
+
+        String sisuHtml = uudis.getSisuHTML();
+
+        StringBuilder uudiseSisu = new StringBuilder();
+        String algusHtmlTag = "<div class=\"text flex-row\">";
+        String lõppHtmlTag = "</div>";
+
+        int index = 0;
+        while ((index = sisuHtml.indexOf(algusHtmlTag, index)) != -1) {
+            int endIndex = sisuHtml.indexOf(lõppHtmlTag, index);
+            if (endIndex != -1) {
+                String htmlInformatsioon = sisuHtml.substring(index + algusHtmlTag.length(), endIndex);
+
+                String algusP = "<p>";
+                String lõppP = "</p>";
+
+                int pIndex = 0;
+                while ((pIndex = htmlInformatsioon.indexOf(algusP, pIndex)) != -1) {
+                    int pEndIndex = htmlInformatsioon.indexOf(lõppP, pIndex);
+                    if (pEndIndex != -1) {
+                        String pSisu = htmlInformatsioon.substring(pIndex + algusP.length(), pEndIndex);
+
+                        pSisu = Utils.lihtsustaHtmlSildid(pSisu);
+
+                        uudiseSisu.append(pSisu).append("\n");
+
+                        pIndex = pEndIndex + lõppP.length();
+                    }
+                }
+
+                index = endIndex + lõppHtmlTag.length();
+            }
+        }
+
+        uudis.setSisu(uudiseSisu.toString());
+    }
+}
---
+++ src/main/java/com/uudiselugeja/uudiselugeja/main.java
@@ -0,0 +1,97 @@
+/**
+    Autor:                   Mihkel Tuisk
+
+    Programmi Kirjeldus:     Saada kiirelt suvalisi uudiseid teatud uudisteportaalidest. Konsoolist pead sisestama millisest
+                             uudiseportaalist saada uudiseid.
+
+    Etapid:                  1. Loo Skraaper klass
+                             2. Loo Utils klass
+                             3. Loo Delfi ja ERR klass
+                             4. Loo Uudis klass
+                             6. Pane kõik kokku main klassis ja testi.
+
+    Liikmete panus:          Mihkel Tuisk 100%
+
+    Ligikaudne Ajakulu:      7h 30min
+
+    Tegemise Mured:          Igal uudiseportaalil on erinev viis kuidas kuvatakse uudiseid html-is
+
+    Hinnang Töö Lõppemusele: 9.2/10. String lihtsustaHtmlSildid(String tekst) vajab veel arendamist, vahepeal satub HTMLi
+                             inimloetavasse teksti sisse.
+
+    Kas Programm Töötab?     Main meetodis on võimal testida kõiki variante. Alguses lõin Skraaperi, et testida kuidas
+                             saada HTML sisu lehekülgedelt, siis lõin Utils classi, et lihtsusdada elu. Pealse seda
+                             testisin kuidas täpsemalt HTMList välja võtta pealkiri ja sisu (Delfi ja ERR class).
+ */
+package com.uudiselugeja.uudiselugeja;
+
+import java.util.List;
+import java.util.Scanner;
+
+public class main {
+    /**
+     * Kuvab suvalise ERR uudise.
+     * Kui ERR-lehelt ei leita ühtegi uudist, kuvatakse veateade.
+     */
+    private static void kuvaSuvalineERRUudis() {
+        ERR ERR = new ERR();
+        List<Uudis> uudised = ERR.leiaKõikUudised();
+
+        if (!uudised.isEmpty()) {
+            Uudis uudis = uudised.get(Utils.saaSuvalineArv(uudised.size()));
+            ERR.päriUudiseSisu(uudis);
+            System.out.println(uudis);
+            return;
+        }
+
+        System.out.println("Uudiseid ei leitud.");
+    }
+
+    /**
+     * Kuvab suvalise Delfi uudise.
+     * Kui Delfi lehelt ei leita ühtegi uudist, kuvatakse veateade.
+     */
+    private static void kuvaSuvalineDelfiUudis() {
+        Delfi delfi = new Delfi();
+        List<Uudis> uudised = delfi.leiaKõikUudised();
+
+        if (!uudised.isEmpty()) {
+            Uudis uudis = uudised.get(Utils.saaSuvalineArv(uudised.size()));
+            delfi.päriUudiseSisu(uudis);
+            System.out.println(uudis);
+            return;
+        }
+
+        System.out.println("Uudiseid ei leitud.");
+    }
+
+    /**
+     * Peameetod.
+     *
+     * @throws InterruptedException Kui programm katkestatakse.
+     */
+    public static void main(String[] args) throws InterruptedException {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Siin on uudiseportaalide numbrid, palun vali üks neist, mille suvalist uudist saada:");
+        System.out.println("ERR:   1");
+        System.out.println("Delfi: 2");
+
+        System.out.print("Palun sisesta uudiseportaali number siia: ");
+
+        int number = scanner.nextInt();
+
+        switch (number) {
+            case 1:
+                kuvaSuvalineERRUudis();
+                break;
+            case 2:
+                kuvaSuvalineDelfiUudis();
+                break;
+            default:
+                System.out.println("Vale number. Palun vali 1 (ERR) või 2 (Delfi).");
+        }
+
+        scanner.close();
+    }
+}
